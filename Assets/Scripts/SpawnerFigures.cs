ec0b21997f20519de0b6782ab87cc72451c95a35
arsenicxz <57327489+arsenicxz@users.noreply.github.com>
2021-04-16 20:18:09 +0300

    Add Instantiate and moving figures

--- Assets/Scripts/SpawnerFigures.cs
+++ Assets/Scripts/SpawnerFigures.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FigureLifetime : MonoBehaviour
+public class SpawnerFigures : MonoBehaviour
 {
-    public float speedOfFigure;
+    public GameObject Template;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(Template);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, -Time.deltaTime * speedOfFigure, 0); 
+        
     }
 }
