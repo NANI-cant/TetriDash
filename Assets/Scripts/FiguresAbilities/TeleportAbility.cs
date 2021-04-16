882f4a44351cbfb2d25bb02440af367759f9f663
NANI-cant <trixxx.asd@yandex.ru>
2021-04-16 20:33:57 +0300

    Work on TeleportAbility

--- Assets/Scripts/FiguresAbilities/TeleportAbility.cs
+++ Assets/Scripts/FiguresAbilities/TeleportAbility.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class TeleportAbility : MonoBehaviour
 {
     [SerializeField] private float timeBetweenTeleports;
-    private Collider2D[] allColliders;
-    private Collider2D leftLimit;
-    private Collider2D rightLimit;
+    private Collider2D _collider;
+    private Bounds _bounds;
     private float remainigTimeToTeleport;
 
     private void Start(){
-        allColliders = GetComponents<Collider2D>();
+        _bounds = new Bounds(Vector3.zero,Vector3.zero);
+        for(int i=0;i<transform.childCount;i++){
+            Bounds currentChildBounds = new Bounds(transform.GetChild(i).transform.position,Vector2.one);
+            _bounds.Encapsulate(currentChildBounds);
+        }
+        _collider = GetComponent<Collider2D>();
         remainigTimeToTeleport = timeBetweenTeleports;
     }
 
     private void FixedUpdate(){
         if(remainigTimeToTeleport <= 0){
-            FindPlaceToTeleport();
+            TryToTeleport();
             remainigTimeToTeleport = timeBetweenTeleports;
         }
         else{
@@ -25,11 +29,7 @@ public class TeleportAbility : MonoBehaviour
         }
     }
 
-    private void FindPlaceToTeleport(){
-
-    }
-
-    private void TeleportMe(){
+    private void TryToTeleport(){
 
     }
 }
