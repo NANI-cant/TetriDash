2c4709fb0562fb90d4e904b921d3e84b1f477d72
NANI-cant <trixxx.asd@yandex.ru>
2021-04-16 12:34:43 +0300

    Add some abilities
    Add Rotate ability and start coding teleport ability

---
+++ Assets/Scripts/FiguresAbilities/TeleportAbility.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportAbility : MonoBehaviour
+{
+    [SerializeField] private float timeBetweenTeleports;
+    private Collider2D[] allColliders;
+    private Collider2D leftLimit;
+    private Collider2D rightLimit;
+    private float remainigTimeToTeleport;
+
+    private void Start(){
+        allColliders = GetComponents<Collider2D>();
+        remainigTimeToTeleport = timeBetweenTeleports;
+    }
+
+    private void FixedUpdate(){
+        if(remainigTimeToTeleport <= 0){
+            FindPlaceToTeleport();
+            remainigTimeToTeleport = timeBetweenTeleports;
+        }
+        else{
+            remainigTimeToTeleport-=Time.deltaTime;
+        }
+    }
+
+    private void FindPlaceToTeleport(){
+
+    }
+
+    private void TeleportMe(){
+
+    }
+}
