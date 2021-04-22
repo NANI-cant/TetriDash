159e7a408148734913feb5f81caf29019fc8d8ea
NANI-cant <trixxx.asd@yandex.ru>
2021-04-22 17:43:11 +0300

    Finish Setting of Prefabs

--- Assets/Scripts/FigureMover.cs
+++ Assets/Scripts/FigureMover.cs
@@ -6,13 +6,16 @@ public class FigureMover : MonoBehaviour
 {
     public float SpeedOfFigure;
     private Transform _transform;
+    private Rigidbody2D _rigidbody;
     private void Start()
     {
         _transform = transform;
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        _transform.Translate(Vector2.down * SpeedOfFigure * Time.deltaTime,Space.World); 
+        //_transform.Translate(Vector2.down * SpeedOfFigure * Time.deltaTime,Space.World); 
+        _rigidbody.velocity = Vector2.down * SpeedOfFigure;
     }
 }
