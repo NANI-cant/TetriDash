62ff0c7dcdd9942cdd6917a727a74946a6038b9e
NANI-cant <trixxx.asd@yandex.ru>
2021-04-22 17:34:18 +0300

    Finish SquareConnector

--- Assets/Scripts/PlatformLifter.cs
+++ Assets/Scripts/PlatformLifter.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class PlatformLifter : MonoBehaviour
 {
     [SerializeField] private DestroyerFigures _deadLine;
+    [SerializeField] private float liftingSpeed = 0.3f;
+    private Transform _transform;
+    private float startHeight;
+    private float currentHeight;
 
     private void Start(){
         _deadLine.OnFigureDestroy+=LiftPlatform;
+        _transform = transform;
+        startHeight = currentHeight = _transform.position.y;
+    }
+
+    private void FixedUpdate(){
+        if(_transform.position.y!=currentHeight){
+            float distance = currentHeight - _transform.position.y;
+            transform.Translate(new Vector3(0,distance,0) * liftingSpeed * Time.deltaTime);
+        }
     }
 
     private void LiftPlatform(){
-        Debug.Log("PlatformLiftActivate");
+        currentHeight++;
+    }
+
+    private void LiftDownPlatform(){
+        if(currentHeight>startHeight){
+            currentHeight--;
+        }
     }
 }
--- Assets/Scripts/SquaresConnecter.cs
+++ Assets/Scripts/SquaresConnecter.cs
@@ -4,70 +4,34 @@ using UnityEngine;
 
 public class SquaresConnecter : MonoBehaviour
 {
+    [SerializeField] private GameObject connectedSquare;
     private Transform _platform;
     private bool isPlatform;
-    [SerializeField] private GameObject connectedSquare;
-
     private void Awake(){
         _platform = FindObjectOfType<PlatformMover>().gameObject.transform;
         isPlatform = GetComponent<PlatformMover>()!=null;
     }
 
-    private void Update(){
-        RaycastHit2D[] hitResults1;
-        RaycastHit2D[] hitResults2;
-        RaycastHit2D[] hitResults3;
-        RaycastHit2D[] hitResults4;
-
-        if(isPlatform){
-            hitResults1 = Physics2D.RaycastAll(new Vector2(transform.position.x - 1f,transform.position.y + 0.25f),new Vector2(0,1),0.01f);
-            hitResults2 = Physics2D.RaycastAll(new Vector2(transform.position.x + 0f,transform.position.y + 0.25f),new Vector2(0,1),0.01f);
-            hitResults3 = Physics2D.RaycastAll(new Vector2(transform.position.x + 1f,transform.position.y + 0.25f),new Vector2(0,1),0.01f);
-            hitResults4 = null;
-        }else{
-            hitResults1 = Physics2D.RaycastAll(new Vector2(transform.position.x + 0f,transform.position.y + 0.5f),new Vector2(0,1),0.01f);
-            hitResults2 = Physics2D.RaycastAll(new Vector2(transform.position.x - 0.5f,transform.position.y + 0f),new Vector2(-1,0),0.01f);
-            hitResults3 = Physics2D.RaycastAll(new Vector2(transform.position.x + 0.5f,transform.position.y + 0f),new Vector2(1,0),0.01f);
-            hitResults4 = Physics2D.RaycastAll(new Vector2(transform.position.x + 0f,transform.position.y - 0.5f),new Vector2(0,-1),0.01f);
-        }
-
-        //rewrite this please
-        foreach (RaycastHit2D hitResult in hitResults1){
-            if(hitResult.collider.gameObject.GetComponent<SpriteRenderer>()!=null && hitResult.collider.gameObject.GetComponent<SquaresConnecter>()==null){
-                ConnectFigure(hitResult.collider.gameObject);
-            }
-        }
-
-        foreach (RaycastHit2D hitResult in hitResults2){
-            if(hitResult.collider.gameObject.GetComponent<SpriteRenderer>()!=null && hitResult.collider.gameObject.GetComponent<SquaresConnecter>()==null){
-                ConnectFigure(hitResult.collider.gameObject);
-            }
-        }
-
-        foreach (RaycastHit2D hitResult in hitResults3){
-            if(hitResult.collider.gameObject.GetComponent<SpriteRenderer>()!=null && hitResult.collider.gameObject.GetComponent<SquaresConnecter>()==null){
-                ConnectFigure(hitResult.collider.gameObject);
-            }
-        }
-
-        if(hitResults4!=null){
-            foreach (RaycastHit2D hitResult in hitResults4){
-                if(hitResult.collider.gameObject.GetComponent<SpriteRenderer>()!=null && hitResult.collider.gameObject.GetComponent<SquaresConnecter>()==null){
-                    ConnectFigure(hitResult.collider.gameObject);
-                }
-            }
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.GetComponent<FigureMover>()!=null){
+            ConnectFigure(other.gameObject);
         }
     }
 
-    private void ConnectFigure(GameObject currentFigureSquare){
-        Color currentColor = currentFigureSquare.GetComponent<SpriteRenderer>().color;
-        currentFigureSquare.transform.parent = null;
-        Destroy(currentFigureSquare);
-        GameObject currentSquare = Instantiate(connectedSquare,currentFigureSquare.transform.position,Quaternion.identity, _platform);
-        Vector3 roundedPosition = currentSquare.transform.localPosition;
-        roundedPosition.x = Mathf.RoundToInt(roundedPosition.x);
-        roundedPosition.y = Mathf.RoundToInt(roundedPosition.y);
-        currentSquare.transform.localPosition = roundedPosition;
-        currentSquare.GetComponent<SpriteRenderer>().color = currentColor;
+    private void ConnectFigure(GameObject currentFigure){
+        Vector3[] squaresPositions = new Vector3[4];
+        for(int i=0;i<squaresPositions.Length;i++){
+            squaresPositions[i] = currentFigure.transform.GetChild(i).transform.position;
+        }
+        Color currentColor = currentFigure.GetComponentInChildren<SpriteRenderer>().color;
+        Destroy(currentFigure);
+        for(int i=0;i<squaresPositions.Length;i++){
+            GameObject currentSquare = Instantiate(connectedSquare,squaresPositions[i],Quaternion.identity,_platform);
+            Vector3 roundedPosition = currentSquare.transform.localPosition;
+            roundedPosition.x = Mathf.RoundToInt(roundedPosition.x);
+            roundedPosition.y = Mathf.RoundToInt(roundedPosition.y);
+            currentSquare.transform.localPosition = roundedPosition;
+            currentSquare.GetComponent<SpriteRenderer>().color = currentColor;
+        }
     }
 }
