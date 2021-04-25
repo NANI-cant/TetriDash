beff6a20f765b72845d1c3304efdcdb0e08d1729
NANI-cant <trixxx.asd@yandex.ru>
2021-04-25 14:47:13 +0300

    Fix Teleport and Accelerate

--- Assets/Scripts/FiguresAbilities/AccelerateAbility.cs
+++ Assets/Scripts/FiguresAbilities/AccelerateAbility.cs
@@ -8,24 +8,21 @@ public class AccelerateAbility : MonoBehaviour
     [SerializeField] private float acceleration;
     [SerializeField] private float minTimeToAcceleration;
     [SerializeField] private float maxTimeToAcceleration;
-    private Transform _transform;
-    private Bounds _bounds;
+    private Collider2D _collider;
     private FigureMover _figureMover;
     private float remainingTimeToAcceleration;
     private bool isAccelerate = false;
 
     private void Start(){
-        _transform = transform;
         _figureMover = GetComponent<FigureMover>();
+        _collider = GetComponent<Collider2D>();
         remainingTimeToAcceleration = Random.Range(minTimeToAcceleration,maxTimeToAcceleration);
     }
 
     private void FixedUpdate(){
         if(!isAccelerate){
             if(remainingTimeToAcceleration<=0){
-                if(CheckIsWayFree()){
-                    Accelerate();
-                }
+                Accelerate();
             }else{
                 remainingTimeToAcceleration-=Time.deltaTime;
             }
@@ -35,21 +32,5 @@ public class AccelerateAbility : MonoBehaviour
     private void Accelerate(){
         _figureMover.SpeedOfFigure+=acceleration;
         isAccelerate = true;
-    }
-
-    private bool CheckIsWayFree(){
-         _bounds = new Bounds();
-        for(int i=0;i<_transform.childCount;i++){
-            Bounds currentChildBounds = new Bounds(_transform.GetChild(i).transform.position,Vector2.one);
-            _bounds.Encapsulate(currentChildBounds);
-        }
-        RaycastHit2D[] hitResults;
-        hitResults = Physics2D.BoxCastAll(_bounds.center,_bounds.size,0,new Vector2(0,-1));
-        foreach (RaycastHit2D hitResult in hitResults){
-            if(hitResult.transform.gameObject.GetComponent<FigureMover>()!=null){
-                return false;
-            }
-        }
-        return true;
     }
 }
--- Assets/Scripts/FiguresAbilities/TeleportAbility.cs
+++ Assets/Scripts/FiguresAbilities/TeleportAbility.cs
@@ -9,40 +9,47 @@ public class TeleportAbility : MonoBehaviour
     public float maxTimeToTeleport = 6f;
     private Collider2D _collider;
     private float remainigTimeToTeleport;
-    private float xTeleport;
     private Transform leftSide;
     private Transform rightSide;
     private bool isTeleported = false;
+
+    public float maxLeftTranslate;
+    public float maxRightTranslate;
 
     private void Start(){
         _collider = GetComponent<Collider2D>();
         remainigTimeToTeleport = Random.Range(minTimeToTeleport,maxTimeToTeleport);
         leftSide = GameObject.FindGameObjectWithTag("LeftLimit").transform;
         rightSide = GameObject.FindGameObjectWithTag("RightLimit").transform;
+        maxLeftTranslate = leftSide.position.x-transform.position.x;
+        maxRightTranslate = rightSide.position.x-transform.position.x;
     }
 
     private void FixedUpdate(){
-        if(remainigTimeToTeleport <= 0 && !isTeleported){
-            TryToTeleport();
-        }
-        else{
-            remainigTimeToTeleport-=Time.deltaTime;
+        if(!isTeleported){
+            if(remainigTimeToTeleport <= 0){
+                TryToTeleport();
+            }
+            else{
+                remainigTimeToTeleport-=Time.deltaTime;
+            }
         }
     }
 
     private void TryToTeleport(){
+        float xTeleport = 0;
         do{
-            float maxLeftTranslate = leftSide.position.x-transform.position.x;
-            float maxRightTranslate = rightSide.position.x-transform.position.x;
+            maxLeftTranslate = leftSide.position.x-transform.position.x;
+            maxRightTranslate = rightSide.position.x-transform.position.x;
             xTeleport = Random.Range(maxLeftTranslate,maxRightTranslate);
-        } while (!isAreaFree());
-        transform.Translate(new Vector3(xTeleport,0,0));
+        } while (!isAreaFree(xTeleport));
+        transform.Translate(new Vector3(xTeleport,0f,0f),Space.World);
         isTeleported = true;
     }
 
-    private bool isAreaFree(){
+    private bool isAreaFree(float xTeleport){
         RaycastHit2D[] boxResult;
-        boxResult = Physics2D.BoxCastAll(_collider.bounds.center + new Vector3(xTeleport,0,0),_collider.bounds.size,0,new Vector2(0,0));
+        boxResult = Physics2D.BoxCastAll(_collider.bounds.center + new Vector3(xTeleport,0,0),_collider.bounds.size,0,Vector2.zero);
         if(boxResult.Length>1){
             return false;
         }else if(boxResult.Length==1 && boxResult[0].collider == _collider || boxResult.Length==0){
