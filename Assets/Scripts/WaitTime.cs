c476aaee4178a477e5d3522d11b9e8e94ab546e3
NANI-cant <trixxx.asd@yandex.ru>
2021-07-30 22:35:16 +0300

    add tutor

---
+++ Assets/Scripts/WaitTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WaitTime : MonoBehaviour
+{
+    [SerializeField] private float time = 0f;
+    [SerializeField] private UnityEvent OnTimeWait;
+
+    private void Start()
+    {
+        Invoke(nameof(DoAction), time);
+    }
+
+    private void DoAction()
+    {
+        OnTimeWait?.Invoke();
+    }
+}
