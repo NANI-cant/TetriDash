461b380ae380da6a949a549e33aa111d490b64ef
NANI-cant <trixxx.asd@yandex.ru>
2021-09-02 17:17:08 +0300

    create tutor on engine

--- Assets/Scripts/WaitTime.cs
+++ Assets/Scripts/WaitTime.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.Events;
 
 public class WaitTime : MonoBehaviour
 {
     [SerializeField] private float time = 0f;
     [SerializeField] private UnityEvent OnTimeWait;
+    [SerializeField] private VideoPlayer videoPlayer;
 
     private void Start()
     {
-        Invoke(nameof(DoAction), time);
+        //Invoke(nameof(DoAction), time);
+
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += ctx => videoPlayer.Play();
+    }
+
+    private IEnumerator Play()
+    {
+        while (!videoPlayer.isPrepared)
+        {
+            Debug.Log("Prepared " + videoPlayer.isPrepared.ToString());
+            yield return new WaitForSeconds(3f);
+        }
+        videoPlayer.Play();
+        yield return new WaitForSeconds(3f);
     }
 
     private void DoAction()
